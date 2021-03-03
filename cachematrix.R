557dcc59548c9e647cf9fa4fb300660840980120
Felipe Takaesu <felipe.takaesu@gmail.com>
2021-03-03 11:45:41 -0300

    R ProgrammingAssignment 2 changes

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     In <- matrix()
+     frame <- data.frame(FuncNames=1:4, row.names= c("setMx","getMx","setIn","getIn"))
+     setMx <- function(y){
+          x <<- y
+          In <<- matrix()
+     }
+     getMx <- function(){
+          return(x)
+     }
+     setIn <- function(Inverse) {
+          In <- Inverse
+     }
+     getIn <- function(){
+          return(In)
+     }
+     frame$FuncNames <- c(setMx,getMx,setIn,getIn)
+     frame
+     
+     
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     In <- x[["getIn","FuncNames"]]()
+     if(!is.na(In[[1,1]])){
+          message("getting cached inverse matrix")
+          return(In)
+     }
+     data <- x[["getMx","FuncNames"]]
+     In <- solve(data())
+     x[["setIn","FuncNames"]](In)
+     return(In)
 }
